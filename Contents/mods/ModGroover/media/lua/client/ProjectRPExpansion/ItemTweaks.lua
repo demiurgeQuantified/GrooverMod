5137b771525008b1947688008681046cc5f18c39
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-11-08 01:59:57 +0000

    move tweaks to shared

--- Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/ItemTweaks.lua
+++ Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/ItemTweaks.lua
@@ -1,18 +1,1 @@
-do
-    local scriptManager = getScriptManager()
-
-    local cordlessPhone = scriptManager:getItem('Base.CordlessPhone')
-    cordlessPhone:DoParam('DisplayCategory = Communications')
-    cordlessPhone:DoParam('Type = Radio')
-    cordlessPhone:DoParam('TwoWay = TRUE')
-    cordlessPhone:DoParam('TransmitRange = 99999')
-    cordlessPhone:DoParam('MicRange = 0')
-    cordlessPhone:DoParam('BaseVolumeRange = 0')
-    cordlessPhone:DoParam('IsPortable = TRUE')
-    cordlessPhone:DoParam('IsTelevision = FALSE')
-    cordlessPhone:DoParam('MinChannel = 200')
-    cordlessPhone:DoParam('MaxChannel = 1000000')
-    cordlessPhone:DoParam('UsesBattery = TRUE')
-    cordlessPhone:DoParam('IsHighTier = TRUE')
-    cordlessPhone:DoParam('UseDelta = 0.003')
-end
+return
