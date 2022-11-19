2e6fb67698d6b4a86fdef96fa4d0d40022358ba6
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-11-19 00:19:25 +0000

    fix private zones not updating until restart

--- Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/PrivateZonesDatabase.lua
+++ Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/PrivateZonesDatabase.lua
@@ -3,14 +3,16 @@ require('ProjectRP/ClientDataBase')
 ProjectRP.Client.ClientDataBase.PrivateZones = {}
 
 ProjectRP.Client.ClientDataBase.PrivateZones.UpdatePrivateZone = function(data)
-    local zones = ModData.get('LocalPrivateZones')
+    local zones = ModData.getOrCreate('LocalPrivateZones')
+    local player = getPlayer()
+    local username = player:getUsername()
     for x = data.x1, data.x2 do
         for y = data.y1, data.y2 do
             for z = data.z1, data.z2 do
                 local id = tostring(x) .. '|' .. tostring(y) .. '|' .. tostring(z)
-                local tAccessToContainers = data.accessEveryone.accessToContainers or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[player:getUsername()] and data.accessUsers[player:getUsername()].accessToContainers or data.owner == username
-                local tAccessToGrabDrop = data.accessEveryone.accessToGrabDrop or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[player:getUsername()] and data.accessUsers[player:getUsername()].accessToGrabDrop or data.owner == username
-                local tAccessToAll = data.accessEveryone.accessToAll or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[player:getUsername()] and data.accessUsers[player:getUsername()].accessToAll or data.owner == username
+                local tAccessToContainers = data.accessEveryone.accessToContainers or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[username] and data.accessUsers[username].accessToContainers or data.owner == username
+                local tAccessToGrabDrop = data.accessEveryone.accessToGrabDrop or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[username] and data.accessUsers[username].accessToGrabDrop or data.owner == username
+                local tAccessToAll = data.accessEveryone.accessToAll or ProjectRP.Client.ClientDataBase.HaveProfessionAccessToContainers(player, data) or data.accessUsers[username] and data.accessUsers[username].accessToAll or data.owner == username
                 zones[id] = { accessToContainers = tAccessToContainers, accessToGrabDrop = tAccessToGrabDrop, accessToAll = tAccessToAll }
             end
         end
@@ -18,7 +20,7 @@ ProjectRP.Client.ClientDataBase.PrivateZones.UpdatePrivateZone = function(data)
 end
 
 ProjectRP.Client.ClientDataBase.PrivateZones.RemoveZone = function(data)
-    local zones = ModData.get('LocalPrivateZones')
+    local zones = ModData.getOrCreate('LocalPrivateZones')
     for x = data.x1, data.x2 do
         for y = data.y1, data.y2 do
             for z = data.z1, data.z2 do
