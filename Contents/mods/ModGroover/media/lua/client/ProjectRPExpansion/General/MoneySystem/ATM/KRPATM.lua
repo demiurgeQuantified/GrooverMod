a7710b7b03eaf19c76f28e1a0971abb4133389c6
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-12-17 00:32:11 +0000

    fix money duplication exploit

---
+++ Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/General/MoneySystem/ATM/KRPATM.lua
@@ -0,0 +1,15 @@
+require 'ProjectRP/General/MoneySystem/ATM/KRPATM'
+
+---@param playerObj IsoPlayer
+---@param nnum number
+ProjectRP.Client.Money.ATM.decreaseMoneyBalance = function(playerObj, nnum)
+    playerObj:StopAllActionQueue()
+    sendClientCommand(playerObj, 'ProjectRPDataBase', 'DecreaseMoneyBalance', { tableName = "MoneyBalance", num = nnum } )
+end
+
+---@param playerObj IsoPlayer
+---@param nnum number
+ProjectRP.Client.Money.ATM.increaseMoneyBalance = function(playerObj, nnum)
+    playerObj:StopAllActionQueue()
+    sendClientCommand(playerObj, 'ProjectRPDataBase', 'IncreaseMoneyBalance', { tableName = "MoneyBalance", num = nnum } )
+end
