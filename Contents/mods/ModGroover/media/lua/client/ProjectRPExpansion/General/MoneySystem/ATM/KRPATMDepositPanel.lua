40cf4f9d80a2096af1306274015723431aa58a61
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-12-17 01:54:49 +0000

    fix yet another money duplication exploit

---
+++ Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/General/MoneySystem/ATM/KRPATMDepositPanel.lua
@@ -0,0 +1,19 @@
+require 'ProjectRP/General/MoneySystem/ATM/KRPATMDepositPanel'
+
+---@param item InventoryItem
+function KRPATMDepositPanel:addItemToYourOffer(item)
+    if not instanceof(item:getParent(), 'IsoPlayer') then return end
+    local add = true;
+    for i,v in ipairs(self.depositDatas.items) do
+        if v.item == item then
+            add = false;
+            break;
+        end
+    end
+    if add then
+        self.depositDatas:addItem(item:getName(), item);
+        if #self.depositDatas.items == 1 then
+            self.depositDatas.selected = 1;
+        end
+    end
+end
