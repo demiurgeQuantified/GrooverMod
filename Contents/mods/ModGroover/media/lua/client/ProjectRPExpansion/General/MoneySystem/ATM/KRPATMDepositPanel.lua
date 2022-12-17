3654d8778a6af193f125bf6371c24b9e38766ecb
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2022-12-17 01:59:00 +0000

    check container parent, not item parent

--- Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/General/MoneySystem/ATM/KRPATMDepositPanel.lua
+++ Contents/mods/ModGroover/media/lua/client/ProjectRPExpansion/General/MoneySystem/ATM/KRPATMDepositPanel.lua
@@ -2,7 +2,7 @@ require 'ProjectRP/General/MoneySystem/ATM/KRPATMDepositPanel'
 
 ---@param item InventoryItem
 function KRPATMDepositPanel:addItemToYourOffer(item)
-    if not instanceof(item:getParent(), 'IsoPlayer') then return end
+    if not instanceof(item:getContainer():getParent(), 'IsoPlayer') then return end
     local add = true;
     for i,v in ipairs(self.depositDatas.items) do
         if v.item == item then
